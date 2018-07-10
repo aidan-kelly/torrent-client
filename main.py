eaf1857e42975e42ba314186a97bb23f96326eb5
Aidan Kelly <it.is.aidan@gmail.com>
2018-07-10 17:22:29 -0600

    Added needed imports

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import argparse
 
+#pip3 install bencode.py
+import bencode
+
+#pip3 install requests
+import requests
+
 parser = argparse.ArgumentParser(description='Simple torrent client.')
 parser.add_argument("file", help='The torrent file of the file you want to download.')
 args = parser.parse_args()
